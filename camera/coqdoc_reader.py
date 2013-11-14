010a60ecf1d5a8ab38263618670c5f27bc92b61b
Jason Gross <jgross@mit.edu>
2013-11-14 05:20:53 -0500

    Replace unicode characters with ASCII
    At least for passing to coq.  List taken from
    http://coq.inria.fr/V8.4/refman/Reference-Manual017.html#sec573
    augmented with forall and exists.

--- camera/coqdoc_reader.py
+++ camera/coqdoc_reader.py
@@ -16,33 +16,33 @@ class Coqdoc_Reader(CoqReader):
   """ Reader that reads Coqdoc-produces HTML files, creating a movie maintaining
       the original layout, with state output inlined.
   """
-  
+
   def __init__(self):
     """ Setup: creates an empty movie. """
     CoqReader.__init__(self)
     self._movie = Coqdoc_Movie()
     self._prover = None
     self._coqdoc_tree = None
-    
+
   def add_code(self, code):
-    """ Override of the corresponding method in Reader: makes a 
+    """ Override of the corresponding method in Reader: makes a
         BeautifulSoup tree out of the given Coqdoc document. """
     massage = copy.copy(BeautifulSoup.MARKUP_MASSAGE)
     massage.extend([(re.compile('(name|href)="([^"]*)"'),
           lambda match: match.group(1) + '="' + escape(match.group(2)) +'"')])
     self._coqdoc_tree = BeautifulSoup(code, markupMassage = massage)
-  
+
   def _find_commands(self, div):
-    """ Find the commands. This is a wrapper around parent's parse. """ 
+    """ Find the commands. This is a wrapper around parent's parse. """
     try:
       text = ''.join(div.fetchText(text = True))
     except AttributeError:
       text = div
-    
+
     return self.parse(text)
-  
+
   def _replace_html(self, text):
-    """ Replace HTML entitities by ASCII equivalents. 
+    """ Replace HTML entitities by ASCII equivalents.
         Special entities taken from SF's symbols.v """
     replacements = {"&nbsp;":   " ",
                     "&rarr;":   "->",
@@ -56,22 +56,39 @@ class Coqdoc_Reader(CoqReader):
                     "&harr;":   "<->",
                     "&and;":    "/\\",
                     "&or;":     "\/",
-                    "&#8658;":   "~~>",
-                    "&#8660;":   "<~~>",
-                    "&#8866;":   "|-",
+                    "&#8658;":  "~~>",
+                    "&#8660;":  "<~~>",
+                    "&#8866;":  "|-",
                     }
+    unicode_replacements = {u'\u2260':   u'<>',
+                            u'\u22a2':   u'|-',
+                            u'\u2265':   u'>=',
+                            u'\u2264':   u'<=',
+                            u'\u2227':   u'/\\',
+                            u'\u2228':   u'\\/',
+                            u'\xac':     u'~',
+                            u'\u2190':   u'<-',
+                            u'\u2192':   u'->',
+                            u'\u2194':   u'<->',
+                            u'\xd7':     u'*',
+                            u'\u21d2':   u'=>',
+                            u'\u2200':   u'forall',
+                            u'\u2203':   u'exists',
+                            }
 
     text = unescape(text, replacements)
+    for key, value in unicode_replacements.items():
+      text = text.replace(key, value)
     return text
-  
+
   def _process_code(self, div):
     """ Process a code div, returning a scene and the frames referenced.
     """
     frames = []
-  
+
     scene = Scene()
     scene.set_type("code")
-    
+
     coqdoc = []
     for child in div:
       coqdoc.append(child)
@@ -80,41 +97,41 @@ class Coqdoc_Reader(CoqReader):
         command = self._replace_html(commands[0])
         response = self._prover.send(
                              command.encode(self._coqdoc_tree.originalEncoding))
-        
+
         frame = Coqdoc_Frame(command = command, command_cd = coqdoc,
                            response = response)
         frames.append(frame)
         scene.add_scene(frame)
-        
+
         coqdoc = []
-    
+
     trailing_frame = Coqdoc_Frame(command = ''.join([str(el) for el in coqdoc]),
                                   command_cd = coqdoc,
                                   response = None)
     frames.append(trailing_frame)
     scene.add_scene(trailing_frame)
-    
+
     return frames, scene
-  
+
   def _process_doc(self, div):
     frames = []
     scene = Scene()
     scene.set_type("doc")
     scene.set_attributes(div.attrs)
-    
+
     for child in div:
       try:
         child_name = child.name
       except AttributeError:
         child_name = "text"
         child.text = str(child)
-        
-      if child_name == "div": 
+
+      if child_name == "div":
         if div.get("class") == "doc":
           child_frames, child_scene = self._process_doc(child)
         else:
           child_frames, child_scene = self._process_div(child)
-      
+
       else:
         child_scene = Coqdoc_Frame(command = child.text, command_cd = [child],
                                response = None)
@@ -122,46 +139,46 @@ class Coqdoc_Reader(CoqReader):
 
       frames += child_frames
       scene.add_scene(child_scene)
-          
+
     return frames, scene
-  
+
   def _is_code(self, div):
     """ Test whether the given div is a code div. """
     return div.get("class") and "code" in div.get("class").split()
 
   def _process_div(self, div):
-    """ Given a div element, return a list of frames with the div's content, 
+    """ Given a div element, return a list of frames with the div's content,
         and a scene mimicking its structure.  """
-    
+
     if self._is_code(div):
       return self._process_code(div)
-    else: 
+    else:
       return self._process_doc(div)
-      
-    
+
+
   def make_frames(self, prover):
-    """ Constructor of frames: reads the given HTML file (self.script) and 
+    """ Constructor of frames: reads the given HTML file (self.script) and
         produces a frame per command/comment.
-        
-        - prover: handle to the prover to use to generate output. 
+
+        - prover: handle to the prover to use to generate output.
     """
     self._prover = prover
-    
+
     try:
       self._movie.add_to_title(self._coqdoc_tree.head.title.text)
     except AttributeError:
       self._movie.add_to_title("")
-    
-    
+
+
     body = self._coqdoc_tree.body
-    
+
     if body:
       for div in body.findChildren(name = "div", recursive = False):
         (frames, scene) = self._process_div(div)
-          
+
         for frame in frames:
           self._movie.addFrame(frame)
-          
+
         self._movie.add_scene(scene)
-        
+
     return self._movie
