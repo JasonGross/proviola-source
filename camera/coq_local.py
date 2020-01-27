cbcc2f1205f14fecdafc17573b7c6c5860644b64
Jason Gross <jgross@mit.edu>
2020-01-27 17:58:46 -0500

    Use a different method to read from Coq
    Sometimes it seems that we hang waiting on a read from Coq forever, when
    it's in non-blocking mode.  We now use a different method to do async
    reads from Coq.

--- camera/coq_local.py
+++ camera/coq_local.py
@@ -4,21 +4,36 @@
 import subprocess, shlex
 import time
 # The solutions here are *Nix-specific.
-import fcntl, os 
+import fcntl, os
+from Popen_noblock import Popen_async, Empty
+
+def getline(q):
+  ret = ''
+  cur = ''
+  while cur != '\n':
+    cur = q.get(True)
+    ret += cur
+  return ret
+
+def getall(q, timeout=0.1):
+  ret = ''
+  while True:
+    try:
+      ret += q.get(timeout=timeout)
+    except Empty:
+      return ret
 
 class Coq_Local(object):
   def __init__(self, coqtop = "/usr/bin/coqtop"):
-    """ Open a Coq process. 
+    """ Open a Coq process.
       - coqtop: Location of coqtop executable.
-      - timeout: How long to wait for coqtop to print to stdout. 
+      - timeout: How long to wait for coqtop to print to stdout.
     """
-    self._coqtop = subprocess.Popen(shlex.split(coqtop) + ["-emacs"],
-                                    stdin  = subprocess.PIPE,
-                                    stdout = subprocess.PIPE,
-                                    stderr = subprocess.PIPE)
-    fcntl.fcntl(self._coqtop.stdout, fcntl.F_SETFL, os.O_NONBLOCK) 
-    fcntl.fcntl(self._coqtop.stderr, fcntl.F_SETFL, os.O_NONBLOCK)
-    
+    self._coqtop = Popen_async(shlex.split(coqtop) + ["-emacs"],
+                               stdin  = subprocess.PIPE,
+                               stdout = subprocess.PIPE,
+                               stderr = subprocess.PIPE)
+
     # Clear Coq greeting.
     data = self._read_coq()
     if not data:
@@ -26,32 +41,21 @@ class Coq_Local(object):
 
   def _read_coq(self):
     """ Read data from Coqtop. Read stdout after the  """
-    error = False
-    while not error:
-      try:
-        error = self._coqtop.stderr.read()
-      except IOError:
-        time.sleep(.1)
-   
-    try:
-      output = self._clean(self._coqtop.stdout.read())
-    except IOError:
-      output = ""
-    
-    return output
+    error = getline(self._coqtop.stderr)
+
+    return self._clean(getall(self._coqtop.stdout, timeout=0.1))
 
   def _clean(self, string):
     """ Clean a string. """
     return "".join([c for c in string if ord(c) != 253])
-  
+
   def __del__(self):
     """ Clean up: stop Coq process. """
     self._coqtop.terminate()
-    
+
   def send(self, command):
     """ Send data to Coqtop, returning the result. """
     self._coqtop.stdin.write(command + "\n")
     self._coqtop.stdin.flush()
 
     return self._read_coq()
-    
